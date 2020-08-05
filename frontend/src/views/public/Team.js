e09e4b9eb1e2de4693e51da3c2e60f3e3b6e825a
Davide Calvaresi <imdavide@gmail.com>
2020-08-05 14:34:44 +0200

    updated pictures links and AISLAB description

--- frontend/src/views/public/Team.js
+++ frontend/src/views/public/Team.js
@@ -10,23 +10,23 @@ const people = [
   {
     name: 'Giuseppe',
     surname: 'Albanese',
-    description: 'Proident est do officia non veniam aliquip amet anim exercitation reprehenderit in eu dolore.',
+    description: 'Software Engineer.',
     linkedIn: 'https://www.linkedin.com/in/giuseppe-albanese',
-    picture: 'https://media.licdn.com/dms/image/C5603AQFhdoXAulFlUA/profile-displayphoto-shrink_200_200/0?e=1573084800&v=beta&t=F7EogEUT1KzjsYkYVud9qnBzvrRlHkue4bcH1rEulrA'
+    picture: 'https://www.hevs.ch/media/image/3/thumb_1_1/473a5493.jpg'
   },
   {
     name: 'Davide',
     surname: 'Calvaresi',
-    description: 'Dolor reprehenderit labore excepteur duis irure laboris dolor nisi non quis tempor.',
+    description: 'Senior Researcher.',
     linkedIn: 'https://www.linkedin.com/in/davidecalvaresi',
-    picture: 'https://media.licdn.com/dms/image/C4D03AQHQVUVPyh_fFg/profile-displayphoto-shrink_200_200/0?e=1573084800&v=beta&t=BmYTYoA2XguExRWwolXR_Y4UPDvrHOW-Rxh5dEeuTX8'
+    picture: 'https://www.hevs.ch/media/image/3/thumb_1_1/473a5495.jpg'
   },
   {
     name: 'Michael Ignaz',
     surname: 'Schumacher',
-    description: 'Fugiat nulla labore cupidatat deserunt cupidatat occaecat deserunt nostrud.',
+    description: 'Full Professor and director of the AISLAB research group.',
     linkedIn: 'https://www.linkedin.com/in/michael-ignaz-schumacher',
-    picture: 'https://media.licdn.com/dms/image/C5103AQFYt_5GnOlMyQ/profile-displayphoto-shrink_200_200/0?e=1573084800&v=beta&t=dzS5l0_Y7ckEaHcvD_0tzfENnYz6DhXfln7Y9aQU4hY'
+    picture: 'https://www.hevs.ch/media/image/1/thumb_1_1/044_img_1248.jpg'
   },
 ]
 
@@ -44,17 +44,19 @@ function Team(props) {
         </Typography>
           </Box>
           <Typography variant='body1'>
-            Esse sunt consequat reprehenderit nisi tempor eu veniam officia culpa. Ipsum tempor do veniam officia. Enim sint nisi duis ex ad dolore culpa reprehenderit. Ut adipisicing ea ad reprehenderit duis laboris duis ut aute qui consequat labore officia. Excepteur deserunt sunt consequat minim nisi magna dolore incididunt aliqua enim sunt. Fugiat irure laboris veniam ea laboris.
+            The Applied Intelligent Agents Lab (AISLab) is applying Intelligent Agent and Multiagent techniques to healthcare. Its methodology is bottom-up, thus starting from real requirements in close collaborations with healthcare professionals and actors, trying to find the best available techniques to solve them. The lab activities cover mainly the following areas:
            <br />
-            <br />
-            >Velit labore incididunt velit officia eiusmod proident aliquip dolore et. Cillum ipsum pariatur est sunt deserunt pariatur dolor eu commodo mollit ipsum quis. Est in tempor cillum esse incididunt ut cupidatat dolore incididunt nulla incididunt cillum. Deserunt labore est deserunt nisi labore veniam aute labore.
            <br />
-            <br />
+            - Intelligent agents for Personalized Medicine <br />
+            - Health sensors monitoring and decision support <br />
+            - Semantic data representation, interoperability and blockchain technologies for health data exchange
+           <br />
+           <br />
             Id quis laborum voluptate veniam ipsum aute adipisicing commodo excepteur irure deserunt. Et laborum sint nisi ex consectetur amet ullamco. Id laboris eiusmod ullamco dolore qui et duis ut.
            Esse sunt consequat reprehenderit nisi tempor eu veniam officia culpa. Ipsum tempor do veniam officia. Enim sint nisi duis ex ad dolore culpa reprehenderit. Ut adipisicing ea ad reprehenderit duis laboris duis ut aute qui consequat labore officia. Excepteur deserunt sunt consequat minim nisi magna dolore incididunt aliqua enim sunt. Fugiat irure laboris veniam ea laboris.
            <br />
-            <br />
-            Velit labore incididunt velit officia eiusmod proident aliquip dolore et. Cillum ipsum pariatur est sunt deserunt pariatur dolor eu commodo mollit ipsum quis. Est in tempor cillum esse incididunt ut cupidatat dolore incididunt nulla incididunt cillum. Deserunt labore est deserunt nisi labore veniam aute labore.
+           <br />
+           For those areas, the lab develops solutions in integrating Artificial Intelligence with modern software technologies with a focus on mobile, Web and Semantic Web technologies.
           </Typography>
         </Box>
         <Box>
